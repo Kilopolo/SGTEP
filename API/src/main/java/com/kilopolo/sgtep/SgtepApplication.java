3609131fb0ceb04ca1d3efd9b65f48954570ba47
Pablo Diaz Rubio <pablo23dr@gmail.com>
2024-02-16 19:53:51 +0100

    creada app web react

---
+++ API/src/main/java/com/kilopolo/sgtep/SgtepApplication.java
@@ -0,0 +1,13 @@
+package com.kilopolo.sgtep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgtepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgtepApplication.class, args);
+	}
+
+}
