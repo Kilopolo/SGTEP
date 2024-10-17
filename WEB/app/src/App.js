2c255869dc1034f03e0cce6b0b325dfb719e86fb
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2024-10-17 17:43:24 +0200

    firebase basic

--- WEB/app/src/App.js
+++ WEB/app/src/App.js
@@ -1,24 +1,77 @@
+import GestionarNotas from "./components/notas/GestionarNotas.js";
+import AppIntro from "./components/AppIntro.js";
+
+import { Button } from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+// FIREBASE
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import LoadingLogo from "./components/LoadingLogo.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDXOCg6ubHy4eikn43eGBqOfPBUSVaa528",
+  authDomain: "sgt-ep.firebaseapp.com",
+  projectId: "sgt-ep",
+  storageBucket: "sgt-ep.appspot.com",
+  messagingSenderId: "78620192919",
+  appId: "1:78620192919:web:5b7734af2c241adc5339d2",
+  measurementId: "G-0X10F012N2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+logEvent(analytics, "notification_received");
+
+// Get a list of cities from your database
+async function getUsers(db) {
+  const usersCol = collection(db, "notas");
+  console.log(usersCol);
+  const userSnapshot = await getDocs(usersCol);
+  console.log(userSnapshot);
+  const userList = userSnapshot.docs.map((doc) => doc.data());
+  console.log(userList);
+  return userList;
+}
 
 
-import GestionarNotas from './components/notas/GestionarNotas.js';
-import AppIntro from './components/AppIntro.js';
-
-import { Button } from 'react-bootstrap';
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const db = getFirestore(app);
+      const usersList = await getUsers(db);
+      setUsers(usersList);
+      setLoading(false);
+    };
 
-
-
+    fetchUsers();
+  }, []);
+  if (loading) {
+    return <LoadingLogo></LoadingLogo>;
+  }
   return (
-    <div >
+    <div>
+      <ul>
+        {" "}
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
       <GestionarNotas></GestionarNotas>
-          <Button href="#">Link</Button> 
-          <Button type="submit">Button</Button>{' '}
-          <AppIntro></AppIntro>
-
+      <Button href="#">Link</Button>
+      <Button type="submit">Button</Button> <AppIntro></AppIntro>
     </div>
   );
-}
+};
 
 export default App;
--- WEB/app/src/components/LoadingLogo.js
+++ WEB/app/src/components/LoadingLogo.js
@@ -1,39 +1,32 @@
-
-import React, { useState, useEffect } from 'react';
-import '../stylesheets/general.css';
-import '../stylesheets/LoadingLogo.css';
+import React, { useState, useEffect } from "react";
+import "../stylesheets/general.css";
+import "../stylesheets/LoadingLogo.css";
+import icon from "../img/loading-icon-png-24.png";
 
 function LoadingLogo() {
+  const [currentLogo, setCurrentLogo] = useState("");
 
+  useEffect(() => {
+    const logos = ["loadingLogo1", "loadingLogo2", "loadingLogo3"];
+    // Selecciona un logo aleatoriamente al montar el componente
+    const randomLogo = logos[Math.floor(Math.random() * (logos.length + 1))];
+    setCurrentLogo(randomLogo);
+  }, []); // [] asegura que solo se ejecute al montar el componente
 
-    const [currentLogo, setCurrentLogo] = useState('');
+  return (
+    <div className="loading-container">
+      <div className="logo-spinner">
+        <div alt="Loading Logo"></div>
 
-    useEffect(() => {
-        const logos = [
-            'loadingLogo1',
-            'loadingLogo2',
-            'loadingLogo3'
-           
-        ];
-        // Selecciona un logo aleatoriamente al montar el componente
-        const randomLogo = logos[Math.floor(Math.random() * (logos.length + 1))];
-        setCurrentLogo(randomLogo);
-    }, []); // [] asegura que solo se ejecute al montar el componente
-
-    return (
-        <div className="loading-container">
-            <div className="logo-spinner">
-                <div alt="Loading Logo" className= "logo"></div>
-                
-            {/* <img src="../img/loading-icon-png-24.png" alt="Loading Logo" className="logo" /> */}
-            </div>
-            {/* <img src={currentLogo} alt="Loading Logo"  />  */}
-            <div alt="Loading Logo" className= {currentLogo}>
-                <p>{currentLogo}</p>
-                </div>
-            <p className="loading-text">Loading...</p>
-        </div>
-    );
+        <img src={icon} alt="Loading Logo" className="logo" />
+      </div>
+      {/* <img src={currentLogo} alt="Loading Logo"  />  */}
+      <div alt="Loading Logo" className={currentLogo}>
+        <p>{currentLogo}</p>
+      </div>
+      <p className="loading-text">Loading...</p>
+    </div>
+  );
 }
 
 export default LoadingLogo;
